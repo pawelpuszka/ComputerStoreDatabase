8641b1cb80feb224f8cb34860d49fed85af1087d
Paweł Puszka <pawel.puszka@gmail.com>
2022-05-23 12:52:26 +0200

    changing encoding to ANSI Windows-1250

--- populating computer_store/employees/buffor_table.sql
+++ populating computer_store/employees/buffor_table.sql
@@ -9,3 +9,21 @@ CREATE TABLE buf_Pracownicy(
 	IdDzialu VARCHAR2(40) NOT NULL,
 	IdPrzelozonego VARCHAR2(40)
 )
+
+SELECT * 
+FROM buf_pracownicy
+;
+
+CREATE TABLE transformation_Pracownicy(
+    data_import         DATE,
+    data_source         VARCHAR2(100),
+	IdPracownicy        VARCHAR2(10) NOT NULL,
+	Imie                VARCHAR2(40) NOT NULL,
+	Nazwisko            VARCHAR2(40) NOT NULL,
+	PESEL               VARCHAR2(11) NULL,
+	PensjaPodstawowa    VARCHAR2(40) NOT NULL,
+	Premia              VARCHAR2(40) NOT NULL,
+	DataZatrudnienia    VARCHAR2(40) NOT NULL,
+	IdDzialu            VARCHAR2(40) NOT NULL,
+	IdPrzelozonego      VARCHAR2(40)
+)
