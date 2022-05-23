63cca80da6bea435acf414ed22b94e3718f4a5b7
Paweł Puszka <pawel.puszka@gmail.com>
2022-05-23 12:36:09 +0200

    script creates buffor table in computer_store

---
+++ populating computer_store/employees/buffor_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE buf_Pracownicy(
+	IdPracownicy VARCHAR2(10) NOT NULL,
+	Imie VARCHAR2(40) NOT NULL,
+	Nazwisko VARCHAR2(40) NOT NULL,
+	PESEL VARCHAR2(11) NULL,
+	PensjaPodstawowa VARCHAR2(40) NOT NULL,
+	Premia VARCHAR2(40) NOT NULL,
+	DataZatrudnienia VARCHAR2(40) NOT NULL,
+	IdDzialu VARCHAR2(40) NOT NULL,
+	IdPrzelozonego VARCHAR2(40)
+)
