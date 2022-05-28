e50d9b06d029e9625796839d65345b53131624d0
Paweł Puszka <pawel.puszka@gmail.com>
2022-05-28 21:42:16 +0200

    beffer tables created in populate addresses script

--- populating computer_store/adresses/script_populate_addresses_table.sql
+++ populating computer_store/adresses/script_populate_addresses_table.sql
@@ -1,1 +1,27 @@
 SET SERVEROUTPUT ON;
+
+DROP TABLE buffer_addresses;
+CREATE TABLE buffer_addresses (
+     street         VARCHAR2(100)
+    ,city           VARCHAR2(100)
+    ,postal_code    VARCHAR2(100)
+    ,phone_number   VARCHAR2(100)
+    ,email          VARCHAR2(100)
+);
+
+SELECT *
+FROM buffer_addresses
+;
+
+DROP TABLE buffer_addresses_2;
+CREATE TABLE buffer_addresses_2 (
+     street         VARCHAR2(100)
+    ,city           VARCHAR2(100)
+    ,postal_code    VARCHAR2(100)
+    ,phone_number   VARCHAR2(100)
+    ,email          VARCHAR2(100)
+);
+
+SELECT *
+FROM buffer_addresses_2
+;
