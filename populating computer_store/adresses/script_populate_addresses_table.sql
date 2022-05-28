f270d1e28703f9df2ab5bc687500038e2f9b3526
Paweł Puszka <pawel.puszka@gmail.com>
2022-05-28 22:40:15 +0200

    script_populate_addresses_table; anonymous block adds phone numbers to buffer_address table

--- populating computer_store/adresses/script_populate_addresses_table.sql
+++ populating computer_store/adresses/script_populate_addresses_table.sql
@@ -2,7 +2,8 @@ SET SERVEROUTPUT ON;
 
 DROP TABLE buffer_addresses;
 CREATE TABLE buffer_addresses (
-     street         VARCHAR2(100)
+     addr_id        INTEGER GENERATED ALWAYS AS IDENTITY
+    ,street         VARCHAR2(100)
     ,city           VARCHAR2(100)
     ,postal_code    VARCHAR2(100)
     ,phone_number   VARCHAR2(100)
@@ -12,6 +13,12 @@ CREATE TABLE buffer_addresses (
 SELECT *
 FROM buffer_addresses
 ;
+DELETE buffer_addresses
+WHERE addr_id = 14;
+
+DELETE buffer_addresses
+WHERE phone_number IS NOT NULL;
+
 
 DROP TABLE buffer_addresses_2;
 CREATE TABLE buffer_addresses_2 (
@@ -25,3 +32,40 @@ CREATE TABLE buffer_addresses_2 (
 SELECT *
 FROM buffer_addresses_2
 ;
+
+DROP TABLE phone_nimbers;
+CREATE TABLE phone_nimbers (
+     phone_id       INTEGER GENERATED ALWAYS AS IDENTITY
+    ,phone_number   VARCHAR2(100)
+);
+
+SELECT *
+FROM phone_nimbers
+;
+
+DECLARE
+    CURSOR phones IS
+        SELECT 
+             phone_id
+            ,phone_number
+        FROM phone_nimbers
+        --FOR UPDATE
+        ;
+    
+    v_phone_number phone_nimbers%ROWTYPE;
+BEGIN
+    OPEN phones;
+    LOOP
+        EXIT WHEN phones%ROWCOUNT > 13;
+        FETCH phones INTO v_phone_number;
+        UPDATE buffer_addresses ba
+        SET ba.phone_number = v_phone_number.phone_number
+        WHERE 
+            ba.addr_id = v_phone_number.phone_id
+            ;
+        COMMIT;
+    END LOOP;
+    
+    CLOSE phones;
+END;
+/
