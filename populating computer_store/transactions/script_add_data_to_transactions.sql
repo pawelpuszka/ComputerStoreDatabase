c641d4030058cb8e131f810f71f5c7aa9ce48e42
Paweł Puszka <pawel.puszka@gmail.com>
2022-06-07 11:32:51 +0200

    script adds data to TRANSACTIONS; code divided into procedures; procedures setting transaction, delivery and payment ids added

--- populating computer_store/transactions/script_add_data_to_transactions.sql
+++ populating computer_store/transactions/script_add_data_to_transactions.sql
@@ -14,28 +14,45 @@ IS
     TYPE transaction_type IS TABLE OF transactions%ROWTYPE INDEX BY PLS_INTEGER;
     at_transactions transaction_type;
     
-    PROCEDURE generate_transactions --RETURN delivery_meth_id_type 
-    IS
-        v_delivery_id transactions%ROWTYPE;
-    BEGIN
-        FOR trans_id IN 1..100
-        LOOP
-            at_transactions(trans_id).delivery_method_id := DBMS_RANDOM.value(1, 4);
-            IF at_transactions(trans_id).delivery_method_id = 4 THEN
-                at_transactions(trans_id).payment_method_id := DBMS_RANDOM.value(1, 3);
-            ELSE
-                LOOP
-                    at_transactions(trans_id).payment_method_id := DBMS_RANDOM.value(1, 4);
-                    EXIT WHEN at_transactions(trans_id).payment_method_id != 2;
-                END LOOP;
-            END IF;
-                
-            dbms_output.put_line('payment_id ' || at_transactions(trans_id).payment_method_id || ', delivery_id: ' || at_transactions(trans_id).delivery_method_id);
-        END LOOP;
-    END generate_transactions;
-BEGIN
-    generate_transactions();
+    v_delivery_id   transactions.delivery_method_id%TYPE;
+    v_payment_id    transactions.payment_method_id%TYPE;
     
+    PROCEDURE set_transaction_id(in_id INTEGER) IS
+    BEGIN
+        at_transactions(in_id).transaction_id := in_id;
+    END set_transaction_id;
+    
+    PROCEDURE set_delivery_id(in_id INTEGER) IS
+    BEGIN
+        at_transactions(in_id).delivery_method_id := DBMS_RANDOM.value(1, 4);
+    END set_delivery_id;
+    
+    PROCEDURE set_payment_id(in_id INTEGER) IS
+    BEGIN
+        IF at_transactions(in_id).delivery_method_id = 4 THEN -- 4- jeżeli sprzedaż stacjonarna
+            at_transactions(in_id).payment_method_id := DBMS_RANDOM.value(1, 3); -- nie placimy przelewem
+        ELSE
+            LOOP
+                at_transactions(in_id).payment_method_id := DBMS_RANDOM.value(1, 4);
+                EXIT WHEN at_transactions(in_id).payment_method_id != 2; --jeżeli nie sprzedaż stacjonarna to nie pacimy gotówką
+            END LOOP;
+        END IF;
+    END set_payment_id;
+    
+    PROCEDURE set_employee_id(in_id INTEGER) IS
+    BEGIN
+        
+    END set_employee_id;
+    
+BEGIN
+    FOR next_id IN 1..100
+    LOOP
+        set_transaction_id(next_id);
+        set_delivery_id(next_id);
+        set_payment_id(next_id);
+        
+        dbms_output.put_line('payment_id ' || at_transactions(next_id).payment_method_id || ', delivery_id: ' || at_transactions(next_id).delivery_method_id);
+    END LOOP;
 END generate_transaction_data;
 /
 
