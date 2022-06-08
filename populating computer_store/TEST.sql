40e10bef8098f2c67c494bb6fef988d8915ae156
Paweł Puszka <pawel.puszka@gmail.com>
2022-06-08 13:52:20 +0200

    new entry to gitignore added

--- populating computer_store/TEST.sql
+++ populating computer_store/TEST.sql
@@ -1,64 +1,24 @@
-SELECT COUNT(a.address_id), e.employee_id
-FROM 
-    addresses a
-    LEFT JOIN employees e
-        ON a.address_id = e.address_id
-WHERE
-    e.employee_id IS NULL
-GROUP BY e.employee_id
-;
+SET SERVEROUTPUT ON;
 
-SELECT a.address_id, a.street, a.city, a.phone_number, e.employee_id
-FROM 
-    addresses a
-    LEFT JOIN employees e
-        ON a.address_id = e.address_id
-WHERE
-    e.employee_id IS NULL
-ORDER BY a.address_id
-;
+SELECT
+    hire_date,
+    end_date,
+    position_id
+FROM
+    employees_contracts 
+where position_id in (8,9)
+    ;
 
+select employee_id
+from transactions;
 
-select *
-from sections;
-
-SELECT * FROM employees;
-
-SELECT 
-     e.employee_id
-    ,e.employee_name
-    ,e.employee_surname
-    ,ep.position_name
-    ,s.section_name
-    ,ec.wages
-FROM 
-    employees e
-    INNER JOIN employees_contracts ec
-        ON ec.contract_id = e.contract_id
-    INNER JOIN employee_positions ep
-        ON ep.position_id = ec.position_id
-    INNER JOIN sections s
-        ON s.section_id = ec.section_id
-;
-
-update product_categories
-set CATEGORY_NAME = 'karty muzyczne'
-where category_id = 3;
-
-
-ALTER TABLE employees_contracts drop constraint employee_contracts_check;
-ALTER TABLE employees_contracts ADD CONSTRAINT emp_contracts_dates_check CHECK(hire_date + 183 < end_date);
-insert into employees_contracts (wages, section_id, position_id, hire_date, end_date)
-values(4500, 7, 13, sysdate, to_date('2022-05-15'));
-
-create table products_copy
-AS
-(select * from products)
-;
-
-select p.product_id, pr.product_id
-from products p
-inner join products pr
-on
- p.product_name = pr.product_name
- and p.product_id != pr.product_id;
+SELECT t.employee_id
+FROM transactions t
+INNER JOIN employees e
+    ON t.employee_id = e.employee_id
+INNER JOIN employees_contracts ec
+    ON e.contract_id = ec.contract_id
+WHERE 
+    CAST(t.start_time AS DATE) > ec.hire_date;
+    
+--daty zatrudnienia pracowników którzy obsugują transakcje
