66fc7d6a21ddd899287d4e3b93041bf2c292f597
Paweł Puszka <pawel.puszka@gmail.com>
2022-06-10 14:09:56 +0200

    script adds data to INCOME_INVOICES; internal procedure generating transactions indexes for INCOME_INVOICES added

--- populating computer_store/sales/script_add_data_to_income_invoices.sql
+++ populating computer_store/sales/script_add_data_to_income_invoices.sql
@@ -4,26 +4,51 @@ DROP PROCEDURE generate_income_invoices_data;
 CREATE OR REPLACE 
 PROCEDURE generate_income_invoices_data 
 IS
-    TYPE transactions_type IS TABLE OF transactions%ROWTYPE INDEX BY PLS_INTEGER;
+    TYPE transactions_type IS TABLE OF transactions.transaction_id%TYPE INDEX BY PLS_INTEGER;
     at_transactions         transactions_type;
     at_invoce_transactions  transactions_type;
     
     PROCEDURE get_transactions IS
     BEGIN
-        SELECT *
+        SELECT transaction_id
         BULK COLLECT INTO at_transactions
         FROM transactions;
     END get_transactions;
     
-    PROCEDURE prepare_transactions_for_invoices 
+    PROCEDURE generate_transaction_ids_for_invoices 
     IS
         v_random_index INTEGER;
+        
+        FUNCTION index_exists(in_index INTEGER) RETURN BOOLEAN IS
+        BEGIN
+            FOR idx IN at_invoce_transactions.FIRST..at_invoce_transactions.LAST
+            LOOP
+                IF at_invoce_transactions(idx) IS NULL OR in_index != at_invoce_transactions(idx)  THEN
+                    RETURN FALSE;
+                END IF;
+            END LOOP;
+            RETURN TRUE;
+        END index_exists;
+        
     BEGIN
+        v_random_index := DBMS_RANDOM.value(1, 2000);
+        at_invoce_transactions(1) := v_random_index;
         FOR idx IN 1..700
         LOOP
-            
+            v_random_index := DBMS_RANDOM.value(1, 2000);
+            IF NOT index_exists(v_random_index) THEN
+                at_invoce_transactions(idx) := v_random_index;
+                dbms_output.put_line(idx || ':  ' || at_invoce_transactions(idx));
+            END IF;
         END LOOP;
-    END prepare_transactions_for_invoices;
+    END generate_transaction_ids_for_invoices;
+    
+    
+    
 BEGIN
-
+    generate_transaction_ids_for_invoices();
 END generate_income_invoices_data;
+/
+
+EXECUTE generate_income_invoices_data();
+/
