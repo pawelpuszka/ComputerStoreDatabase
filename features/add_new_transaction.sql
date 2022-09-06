b4ea3d06fc5587c58ed00d21a10902cb83eee1b9
Pawel Puszka <pawel.puszka@gmail.com>
2022-09-06 11:11:12 +0200

    code formatting

--- features/add_new_transaction.sql
+++ features/add_new_transaction.sql
@@ -1,11 +1,11 @@
 /*
-    File contains the package for creating the entire process of purchasing products in the store.
-    Procedures from that package provide an interface to the client application.
+    file contains the package for creating the entire process of purchasing products in the store.
+    procedures from that package provide an interface to the client application.
     
     file ver.: 1.0 
-    author: Pawe³ Puszka
+    author: pawe³ puszka
     
-    ChangeLog
+    changelog
         20.08.2022 
         - package specification and body created
         - procedure start_new_transaction
@@ -17,52 +17,52 @@
 
 CREATE OR REPLACE
 PACKAGE transaction_pkg IS
-    PROCEDURE start_new_transaction(employee_id_in          transactions.employee_id%TYPE 
-                                   ,payment_method_id_in    transactions.payment_method_id%TYPE
-                                   ,delivery_method_id_in   transactions.delivery_method_id%TYPE
-                                   );
+PROCEDURE start_new_transaction(employee_id_in              transactions.employee_id%TYPE 
+                                                ,payment_method_id_in   transactions.payment_method_id%TYPE
+                                                ,delivery_method_id_in   	transactions.delivery_method_id%TYPE
+                                                );
     PROCEDURE create_products_list;
     PROCEDURE finish_transaction;
 END transaction_pkg;
 /
 
 CREATE OR REPLACE
-PACKAGE BODY transaction_pkg IS
-
-constraint_violation_ex         EXCEPTION;
-    PRAGMA EXCEPTION_INIT(constraint_violation_ex, -02291);
-    insert_null_ex              EXCEPTION;
-    PRAGMA EXCEPTION_INIT(insert_null_ex, -01400);
-    incorrect_transact_ex       EXCEPTION;
-    PRAGMA EXCEPTION_INIT(incorrect_transact_ex, -20012);
-    online_seller_needed_ex     EXCEPTION;
-    PRAGMA EXCEPTION_INIT(online_seller_needed_ex, -20010);
-    stationary_seller_needed_ex EXCEPTION;
-    PRAGMA EXCEPTION_INIT(stationary_seller_needed_ex, -20011);
+PACKAGE BODY transaction_pkg 
+IS
+    constraint_violation_ex         	EXCEPTION;
+    pragma exception_init(constraint_violation_ex, -02291);
+    insert_null_ex              			EXCEPTION;
+    pragma exception_init(insert_null_ex, -01400);
+    incorrect_transact_ex       		EXCEPTION;
+    pragma exception_init(incorrect_transact_ex, -20012);
+    online_seller_needed_ex    		EXCEPTION;
+    pragma exception_init(online_seller_needed_ex, -20010);
+    stationary_seller_needed_ex 	EXCEPTION;
+    pragma exception_init(stationary_seller_needed_ex, -20011);
     
     v_transact_id           transactions.transaction_id%TYPE;
  
 
-    PROCEDURE start_new_transaction(employee_id_in          transactions.employee_id%TYPE 
-                                   ,payment_method_id_in    transactions.payment_method_id%TYPE
-                                   ,delivery_method_id_in   transactions.delivery_method_id%TYPE
-                                   )
+   PROCEDURE start_new_transaction(employee_id_in               transactions.employee_id%TYPE 
+                                                    ,payment_method_id_in   transactions.payment_method_id%TYPE
+                                                    ,delivery_method_id_in   	transactions.delivery_method_id%TYPE
+                                                    )
     IS
-        stationary_sale     CONSTANT INTEGER := 4;
-        cash_payment        CONSTANT INTEGER := 2;
-        transfer_payment    CONSTANT INTEGER := 4;
-        online_seller       CONSTANT INTEGER := 9;        
+        stationary_sale     	CONSTANT integer := 4;
+        cash_payment        	CONSTANT integer := 2;
+        transfer_payment    CONSTANT integer := 4;
+        online_seller       		CONSTANT integer := 9;        
     
         FUNCTION is_online_transaction(delivery_method_id_in    transactions.delivery_method_id%TYPE
-                                      ,payment_method_id_in     transactions.payment_method_id%TYPE 
-                                       ) RETURN BOOLEAN IS
+                                                  ,payment_method_id_in     transactions.payment_method_id%TYPE 
+                                                   ) RETURN BOOLEAN IS
         BEGIN
             RETURN delivery_method_id_in != stationary_sale AND payment_method_id_in != cash_payment;
         END is_online_transaction;
         
         FUNCTION is_stationary_transaction(delivery_method_id_in    transactions.delivery_method_id%TYPE
-                                          ,payment_method_id_in     transactions.payment_method_id%TYPE 
-                                           ) RETURN BOOLEAN IS
+                                                          ,payment_method_id_in     transactions.payment_method_id%TYPE 
+                                                           ) RETURN BOOLEAN IS
         BEGIN
             RETURN delivery_method_id_in = stationary_sale AND payment_method_id_in != transfer_payment;
         END is_stationary_transaction;
@@ -74,7 +74,7 @@ constraint_violation_ex         EXCEPTION;
             SELECT position_id
             INTO v_position_id
             FROM employees_contracts ec
-                INNER JOIN employees e 
+                INNER JOIN employees E 
                     ON  ec.contract_id = e.contract_id
             WHERE e.employee_id = employee_id_in
             ;
@@ -84,15 +84,15 @@ constraint_violation_ex         EXCEPTION;
         
         
         PROCEDURE insert_transaction_data(employee_id_in          transactions.employee_id%TYPE 
-                                        ,payment_method_id_in    transactions.payment_method_id%TYPE
-                                        ,delivery_method_id_in   transactions.delivery_method_id%TYPE
-                                        ) IS
+                                                            ,payment_method_id_in    transactions.payment_method_id%TYPE
+                                                            ,delivery_method_id_in   transactions.delivery_method_id%TYPE
+                                                            ) IS
         BEGIN
             INSERT INTO transactions(employee_id, payment_method_id, delivery_method_id, status_id, start_time)
             VALUES(employee_id_in, payment_method_id_in, delivery_method_id_in, 1, SYSTIMESTAMP)
             RETURNING transaction_id INTO v_transact_id;
             COMMIT;
-            DBMS_OUTPUT.put_line('Transakcja rozpoczêta.' || v_transact_id);
+            dbms_output.put_line('transakcja rozpoczêta.' || v_transact_id);
         END insert_transaction_data;
                 
     BEGIN
@@ -120,29 +120,29 @@ constraint_violation_ex         EXCEPTION;
             ROLLBACK;
             RAISE;
         WHEN incorrect_transact_ex THEN
-            RAISE_APPLICATION_ERROR(-20012, 'Nie mo¿na ustanowic takiej transakcji. B³êdny sposób dostawy lub p³atnoœci.');
+            raise_application_error(-20012, 'nie mo¿na ustanowic takiej transakcji. b³êdny sposób dostawy lub p³atnoœci.');
         WHEN online_seller_needed_ex THEN
-            RAISE_APPLICATION_ERROR(-20010, 'Wybierz odpowiedniego sprzedawcê. Transakcja online.');
+            raise_application_error(-20010, 'wybierz odpowiedniego sprzedawcê. transakcja online.');
         WHEN stationary_seller_needed_ex THEN
-            RAISE_APPLICATION_ERROR(-20011, 'Wybierz odpowiedniego sprzedawcê. Transakcja w sklepie stacjonarnym.');
-        WHEN OTHERS	THEN	
+            raise_application_error(-20011, 'wybierz odpowiedniego sprzedawcê. transakcja w sklepie stacjonarnym.');
+        WHEN others	THEN	
             RAISE;
         
     END start_new_transaction;
     
     
-    PROCEDURE generate_invoice (client_id_in INCOME_INVOICES.WHOLESALE_CLIENT_ID%TYPE)
+    PROCEDURE generate_invoice (client_id_in income_invoices.wholesale_client_id%TYPE)
     IS
         PROCEDURE generate_invoice_number IS
         BEGIN
-            NULL;
+            
         END;
         
     BEGIN
         NULL;
     END	generate_invoice;
     
-    --PROCEDURE get_product
+    --procedure get_product
     
     PROCEDURE create_products_list IS
     BEGIN
