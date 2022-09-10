c559dce0b397c2c0317371abc1b4fbf7fc2a70f5
Pawel Puszka <pawel.puszka@gmail.com>
2022-09-10 22:04:36 +0200

    new package storehouse_service with cursor find_product

--- features/storehouse_service.sql
+++ features/storehouse_service.sql
@@ -5,14 +5,14 @@
 CREATE OR REPLACE	
 PACKAGE storehouse_service
 IS
-
+    CURSOR find_product_cur(prod_id_in products.product_id%TYPE) RETURN products%ROWTYPE;
 END storehouse_service; 
 /
 
 CREATE OR REPLACE	
 PACKAGE BODY storehouse_service
 IS
-    CURSOR find_product_id(prod_id_in products.product_id%TYPE) RETURN products%ROWTYPE IS
+    CURSOR find_product_cur(prod_id_in products.product_id%TYPE) RETURN products%ROWTYPE IS
            SELECT 
                  product_id
                 ,category_id
@@ -20,6 +20,23 @@ IS
                 ,unit_price
             FROM 
                 products
+            WHERE
+                product_id = prod_id_in
+            FOR UPDATE WAIT 3
             ;
+            
+    PROCEDURE decrease_prod_qty(product_id_in     products.product_id%TYPE
+                                                ,product_qty_in    invoice_products_lists.purchased_product_qty%TYPE
+                                                ) 
+    IS
+        v_product_rec find_product_cur%ROWTYPE;
+        is_stationary_sale BOOLEAN := transaction_pkg.v_curr_transact_rec.delivery_method_id = 4;
+    BEGIN
+        OPEN find_product_cur(product_id_in);
+        FETCH find_product_cur INTO v_product_rec;
+        IF is_stationary_sale THEN
+           
+        END IF;
+    END update_stock;
 END storehouse_service; 
 /
