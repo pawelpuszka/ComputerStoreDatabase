a2b3470a7ef81cea649bf2f7e7742ede5334de62
Pawel Puszka <pawel.puszka@gmail.com>
2023-05-26 22:41:34 +0200

    procedure for exceptions logging created in package

--- PACKAGES/pkg_exception_handling_body.sql
+++ PACKAGES/pkg_exception_handling_body.sql
@@ -1,13 +1,19 @@
 CREATE OR REPLACE PACKAGE BODY pkg_exception_handling
 IS
-    PROCEDURE log_exception( code           NUMBER
-                            ,message        VARCHAR2(4000)
-                            ,object_name    VARCHAR2(200)
-                            ,ex_date        DATE )
+    PROCEDURE log_exception( in_code            IN NUMBER
+                            ,in_message         IN VARCHAR2
+                            ,in_object_name     IN VARCHAR2
+                            ,in_ex_date         IN DATE )
     IS
-
+        PRAGMA AUTONOMOUS_TRANSACTION;
     BEGIN
-        INSERT INTO
+        INSERT INTO exception_logs(code, message, object_name, ex_date)
+        VALUES (in_code, in_message, in_object_name, in_ex_date);
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20100, 'There is a problem with exception logging.');
     END log_exception;
 
 END pkg_exception_handling;
+/
