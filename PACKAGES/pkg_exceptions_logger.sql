bc40fa652fa35f488f077346874bb52221d825a1
Pawel Puszka <pawel.puszka@gmail.com>
2023-05-29 23:43:57 +0200

    amendments then compiling package specification

--- PACKAGES/pkg_exceptions_logger.sql
+++ PACKAGES/pkg_exceptions_logger.sql
@@ -5,17 +5,7 @@ IS
                             ,in_object_name     IN VARCHAR2
                             ,in_ex_date         IN DATE);
 
-    PROCEDURE add_employee(  in_name          IN EMPLOYEES.EMPLOYEE_NAME%TYPE
-                            ,in_surname       IN EMPLOYEES.EMPLOYEE_SURNAME%TYPE
-                            ,in_pesel         IN EMPLOYEES.PESEL%TYPE
-                            ,in_email         IN EMPLOYEES.EMAIL%TYPE
-                            ,in_wages         IN EMPLOYEES_CONTRACTS.WAGES%TYPE
-                                --,in_section_id    IN EMPLOYEES_CONTRACTS.SECTION_ID%TYPE
-                            ,in_position_id   IN EMPLOYEES_CONTRACTS.POSITION_ID%TYPE
-                            ,in_street        IN ADDRESSES.STREET%TYPE
-                            ,in_city          IN ADDRESSES.CITY%TYPE
-                            ,in_postal_code   IN ADDRESSES.POSTAL_CODE%TYPE
-                            ,in_phone_no      IN ADDRESSES.PHONE_NUMBER%TYPE);
+
 
 END pkg_exception_handling;
 /
